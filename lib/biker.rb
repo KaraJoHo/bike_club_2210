a76c3fe84471e8327f26a307fe804b1777eeedc8
KaraJH <karas.jones@yahoo.com>
2022-11-15 10:47:14 -0700

    Creates personal_record method for biker class

--- lib/biker.rb
+++ lib/biker.rb
@@ -13,20 +13,44 @@ class Biker
   end
 
   def log_ride(ride, time)
-    #bikers can log a ride with a time
-    #those are passed in to this method
-    #the ride object can track the amount of times
-    #in an array
-    #so the ride object is the Key
-    #the times are the values in an array
+  #biker does not log a ride, if their terrain, is not acceptable terrain
+  ## and if the ride's total distance is greater than the bikers max
 
-    #we have the rides hash, and we can put the
-    #ride object oasswed in as a key, in to the hash
-    #and its values each time should be added to the array
-    if @rides[ride] == nil
-      @rides[ride] = [time]
-    else ride.name == ride.name
-      @rides[ride] += [time]
+    #return unless ride.terrain.include?(@acceptable_terrain) && (ride.total_distance > (@max_distance))
+    # if (ride.terrain == @acceptable_terrain) && (ride.total_distance < @max_distance)
+    #problem : ride.terrain.include? notavailable for symbol
+    if (ride.total_distance < @max_distance)
+      if @rides[ride] == nil
+        @rides[ride] = [time]
+      else ride.name == ride.name
+        @rides[ride] += [time]
+      end
+    end
+  end
+
+  def personal_record(ride_obj)
+    #bikers lowest time recorded for the ride, is their personal_record
+    #if the biker hasn't completed the ride, it returns false
+    #a biker can't completed a ride if the total distance is above their max
+    #or if their terrain is not included in the ride's terrain
+
+    #we have the rides hash which has the ride, and the array of times
+    #we want to check that array for each ride, and return the lowest number
+
+    #iterate over the hash, and get in to the times value, return the lowest value
+
+    #I am getting an array with the lowest time and a nil. i think the
+    #nil is returning in the array because I have it trying to match
+    #the name of the specific ride. I need to come back to see how to get
+    #just the minimum value out of the array
+    if ride_obj.total_distance < @max_distance
+      @rides.map do |ride, time|
+        if ride_obj.name == ride.name
+          time.min
+        end
+      end.compact.pop
+    else
+      false
     end
   end
 end
