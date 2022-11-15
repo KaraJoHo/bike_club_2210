d61ec3d6c90079a8f3d7ca0cdabba7f4e31ecd59
KaraJH <karas.jones@yahoo.com>
2022-11-15 11:24:57 -0700

    Creates test for best time for ride method

--- spec/bikeclub_spec.rb
+++ spec/bikeclub_spec.rb
@@ -46,4 +46,29 @@ RSpec.describe BikeClub do
       expect(bike_club.biker_with_most_rides).to eq(biker)
     end
   end
+
+  describe 'best_time_for_ride' do
+    it 'can show which biker has the best time for a given ride' do
+      bike_club = BikeClub.new("The Bike Club")
+      biker = Biker.new("Kenny", 30)
+      biker2 = Biker.new("Athena", 15)
+
+      ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills})
+      ride2 = Ride.new({name: "Town Lake", distance: 14.9, loop: true, terrain: :gravel})
+
+      biker.log_ride(ride1, 92.5)
+      biker.log_ride(ride1, 91.1)
+      biker.log_ride(ride2, 60.9)
+      biker.log_ride(ride2, 61.6)
+
+      biker2.log_ride(ride2, 65.0)
+      biker2.log_ride(ride2, 40.0)
+
+      bike_club.add_biker(biker)
+      bike_club.add_biker(biker2)
+
+      expect(best_time_for_ride(ride2)).to eq(biker2)
+
+    end
+  end
 end
