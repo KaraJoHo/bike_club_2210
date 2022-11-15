c15c2feac06273fe45f9c61dee81e26c32cda4e0
KaraJH <karas.jones@yahoo.com>
2022-11-15 09:39:34 -0700

    Creates first part of log ride method

--- lib/biker.rb
+++ lib/biker.rb
@@ -11,4 +11,22 @@ class Biker
   def learn_terrain!(terrain)
     @acceptable_terrain << terrain
   end
+
+  def log_ride(ride, time)
+    #bikers can log a ride with a time
+    #those are passed in to this method
+    #the ride object can track the amount of times
+    #in an array
+    #so the ride object is the Key
+    #the times are the values in an array
+
+    #we have the rides hash, and we can put the
+    #ride object oasswed in as a key, in to the hash
+    #and its values each time should be added to the array
+    if @rides[ride] == nil
+      @rides[ride] = [time]
+    else ride.name == ride.name
+      @rides[ride] += [time]
+    end
+  end
 end
