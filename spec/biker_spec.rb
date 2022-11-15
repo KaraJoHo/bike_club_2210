443648d672d97f94ac719f71ad6d0d624df8738d
KaraJH <karas.jones@yahoo.com>
2022-11-15 10:13:29 -0700

    Creates test for personal record method

--- spec/biker_spec.rb
+++ spec/biker_spec.rb
@@ -39,4 +39,35 @@ RSpec.describe Biker do
       expect(biker.rides).to eq(expected)
     end
   end
+
+  describe '#personal_record' do
+    it 'can report its personal record and return false if ride wasnt completed' do
+      biker = Biker.new("Kenny", 30)
+      ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills})
+      ride2 = Ride.new({name: "Town Lake", distance: 14.9, loop: true, terrain: :gravel})
+      biker.log_ride(ride1, 92.5)
+      biker.log_ride(ride1, 91.1)
+      biker.log_ride(ride2, 60.9)
+      biker.log_ride(ride2, 61.6)
+
+      expect(biker.personal_record(ride1)).to eq(91.1)
+      expect(biker.personal_record(ride2)).to eq(60.9)
+
+      # biker2 = Biker.new("Athena", 15)
+      # biker2.log_ride(ride1, 97.0)
+      # biker2.log_ride(ride2, 67.0)
+      #
+      # expect(biker2.rides).to eq({})
+      # biker2.learn_terrain!(:gravel)
+      # biker2.learn_terrain!(:hills)
+      #
+      # biker2.log_ride(ride1, 95.0)
+      # biker2.log_ride(ride2, 65.0)
+      #
+      # expect(biker2.rides).to eq({ride2 => 65})
+      # expect(biker2.personal_record(ride2)).to eq(65.0)
+      # expect(biker2.personal_record(ride1)).to eq(false)
+
+    end
+  end
 end
