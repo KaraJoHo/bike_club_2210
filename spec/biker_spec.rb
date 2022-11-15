1a708c547fbb3a2b000317391965bbcb03285281
KaraJH <karas.jones@yahoo.com>
2022-11-15 09:26:59 -0700

    Tests for acceptable terrain and learn terrain method

--- spec/biker_spec.rb
+++ spec/biker_spec.rb
@@ -8,5 +8,19 @@ RSpec.describe Biker do
     expect(biker.name).to eq("Kenny")
     expect(biker.max_distance).to eq(30)
     expect(biker.rides).to eq({})
+    expect(biker.acceptable_terrain).to eq([])
+  end
+
+  describe '#learn_terrain!' do
+    it 'can list acceptable terrain and learn new terrain' do
+      biker = Biker.new("Kenny", 30)
+
+      expect(biker.acceptable_terrain).to eq([])
+
+      biker.learn_terrain!(:gravel)
+      biker.learn_terrain(:hills)
+
+      expect(biker.acceptable_terrain).to eq([:gravel, :hills])
+    end
   end
 end
