28634c264669ea5b16ee0f89321cd3e720082681
KaraJH <karas.jones@yahoo.com>
2022-11-15 11:58:47 -0700

    Tries to fix error in log ride

--- lib/biker.rb
+++ lib/biker.rb
@@ -18,8 +18,9 @@ class Biker
 
     #return unless ride.terrain.include?(@acceptable_terrain) && (ride.total_distance > (@max_distance))
     # if (ride.terrain == @acceptable_terrain) && (ride.total_distance < @max_distance)
+
     #problem : ride.terrain.include? notavailable for symbol
-    if (ride.total_distance < @max_distance)
+    if (ride.total_distance < @max_distance) #&& ride.terrain.include?(@acceptable_terrain)
       if @rides[ride] == nil
         @rides[ride] = [time]
       else ride.name == ride.name
@@ -29,20 +30,7 @@ class Biker
   end
 
   def personal_record(ride_obj)
-    #bikers lowest time recorded for the ride, is their personal_record
-    #if the biker hasn't completed the ride, it returns false
-    #a biker can't completed a ride if the total distance is above their max
-    #or if their terrain is not included in the ride's terrain
-
-    #we have the rides hash which has the ride, and the array of times
-    #we want to check that array for each ride, and return the lowest number
-
-    #iterate over the hash, and get in to the times value, return the lowest value
-
-    #I am getting an array with the lowest time and a nil. i think the
-    #nil is returning in the array because I have it trying to match
-    #the name of the specific ride. I need to come back to see how to get
-    #just the minimum value out of the array
+    #same issue as above, can't get past the undefined method .include? for :hills symbol
     if ride_obj.total_distance < @max_distance
       @rides.map do |ride, time|
         if ride_obj.name == ride.name
