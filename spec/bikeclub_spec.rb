df62f49b85eceeb2afe1ddc6f4ddc2826195e291
KaraJH <karas.jones@yahoo.com>
2022-11-15 11:48:34 -0700

    Creates test for eligible biker

--- spec/bikeclub_spec.rb
+++ spec/bikeclub_spec.rb
@@ -67,8 +67,32 @@ RSpec.describe BikeClub do
       bike_club.add_biker(biker)
       bike_club.add_biker(biker2)
 
-      expect(best_time_for_ride(ride2)).to eq(biker2)
+      expect(bike_club.best_time_for_ride(ride2)).to eq(biker2)
 
+    end
+  end
+
+  describe '#eligible_biker' do
+    it 'can show which biker is eligible for a ride based on terrain and max distance' do
+      bike_club = BikeClub.new("The Bike Club")
+      biker = Biker.new("Kenny", 30)
+      biker2 = Biker.new("Athena", 15)
+
+      ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills})
+      ride2 = Ride.new({name: "Town Lake", distance: 14.9, loop: true, terrain: :gravel})
+
+      biker.log_ride(ride1, 92.5)
+      biker.log_ride(ride1, 91.1)
+      biker.log_ride(ride2, 60.9)
+      biker.log_ride(ride2, 61.6)
+
+      biker2.log_ride(ride2, 65.0)
+      biker2.log_ride(ride2, 40.0)
+
+      bike_club.add_biker(biker)
+      bike_club.add_biker(biker2)
+
+      expect(eligible_biker(ride1)).to eq([biker1])
     end
   end
 end
