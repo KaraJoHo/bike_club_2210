d42aec6b12ac667335141cb17933a092d21602f0
KaraJH <karas.jones@yahoo.com>
2022-11-15 11:22:35 -0700

    Creates biker with most rides method

--- lib/bikeclub.rb
+++ lib/bikeclub.rb
@@ -9,4 +9,12 @@ class BikeClub
   def add_biker(biker_obj)
     @bikers << biker_obj
   end
+
+  def biker_with_most_rides
+    @bikers.max_by do |biker|
+      biker.rides.map do |ride, time|
+        time.count
+      end.sum
+    end
+  end
 end
