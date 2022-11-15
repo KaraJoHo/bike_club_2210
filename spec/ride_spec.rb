67502d510ba1d7a88dbd6cf3162e563abf8016d9
KaraJH <karas.jones@yahoo.com>
2022-11-15 09:15:30 -0700

    Tests for total distance method

--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -12,15 +12,19 @@ RSpec.describe Ride do
   end
 
   describe '#loop?' do
-    it 'returns true or false it the ride is a loop' do
+    it 'returns true or false if the ride is a loop' do
       ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills})
 
       expect(ride1.loop?).to eq(false)
+
+      ride2 = Ride.new({name: "Town Lake", distance: 14.9, loop: true, terrain: :gravel})
+
+      expect(ride2.loop?).to eq(true)
     end
   end
 
   describe '#total_distance' do
-    xit 'returns the total distance of the ride' do
+    it 'returns the total distance of the ride' do
       ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills})
 
       expect(ride1.total_distance).to eq(21.4)
