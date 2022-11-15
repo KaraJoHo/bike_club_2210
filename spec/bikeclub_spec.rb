981a03a5776c27528a311eae2f3913562d42d86e
KaraJH <karas.jones@yahoo.com>
2022-11-15 10:58:46 -0700

    Creates add biker method test

--- spec/bikeclub_spec.rb
+++ spec/bikeclub_spec.rb
@@ -4,10 +4,23 @@ require './lib/bikeclub'
 
 RSpec.describe BikeClub do
   it 'exists and has a name, and an array for biker objects' do
-    bikeclub = BikeClub("The Bike Club")
+    bike_club = BikeClub.new("The Bike Club")
 
-    expect(bikeclub).to be_a(BikeClub)
-    expect(bikeclub.name).to eq("The Bike Club")
-    expect(bikeclub.bikers).to eq([])
+    expect(bike_club).to be_a(BikeClub)
+    expect(bike_club.name).to eq("The Bike Club")
+    expect(bike_club.bikers).to eq([])
+  end
+
+  describe '#add_biker' do
+    it 'can add bikers to the bikers array' do
+      bike_club = BikeClub.new("The Bike Club")
+      biker = Biker.new("Kenny", 30)
+      biker2 = Biker.new("Athena", 15)
+
+      bike_club.add_biker(biker)
+      bike_club.add_biker(biker2)
+
+      expect(bike_club.bikers).to eq[biker, biker2]
+    end
   end
 end
